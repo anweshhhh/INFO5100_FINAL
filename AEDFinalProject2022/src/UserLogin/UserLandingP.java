b16f67e4c33f2c080be515c4435732df216759df
Anamika Verma <verma.anam@northeastern.edu>
2022-12-06 23:16:53 -0500

    search jobs, view applications and view appointments functionalities added for user

--- AEDFinalProject2022/src/UserLogin/UserLandingP.java
+++ AEDFinalProject2022/src/UserLogin/UserLandingP.java
@@ -26,21 +26,188 @@ public class UserLandingP extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jSplitPane1 = new javax.swing.JSplitPane();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        userSearchJobs = new javax.swing.JButton();
+        userViewApplications = new javax.swing.JButton();
+        userAppointments = new javax.swing.JButton();
+        userLogout = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jSplitPane1.setBackground(new java.awt.Color(0, 0, 0));
+        jSplitPane1.setDividerLocation(300);
+        jSplitPane1.setDividerSize(0);
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel3.setBackground(new java.awt.Color(153, 153, 153));
+
+        jLabel2.setFont(new java.awt.Font("Tamil MN", 1, 14)); // NOI18N
+        jLabel2.setText("Welcome, User!");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(501, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addGap(57, 57, 57))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 561, Short.MAX_VALUE))
+        );
+
+        jSplitPane1.setRightComponent(jPanel2);
+
+        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
+
+        jLabel1.setFont(new java.awt.Font("Telugu MN", 1, 18)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Student Hub");
+
+        userSearchJobs.setBackground(new java.awt.Color(255, 51, 51));
+        userSearchJobs.setFont(new java.awt.Font("Tamil MN", 1, 14)); // NOI18N
+        userSearchJobs.setText("Search Jobs");
+        userSearchJobs.setPreferredSize(new java.awt.Dimension(180, 24));
+        userSearchJobs.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                userSearchJobsActionPerformed(evt);
+            }
+        });
+
+        userViewApplications.setBackground(new java.awt.Color(255, 51, 51));
+        userViewApplications.setFont(new java.awt.Font("Tamil MN", 1, 14)); // NOI18N
+        userViewApplications.setText("View Job Applications");
+        userViewApplications.setSize(new java.awt.Dimension(78, 23));
+        userViewApplications.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                userViewApplicationsActionPerformed(evt);
+            }
+        });
+
+        userAppointments.setBackground(new java.awt.Color(255, 51, 51));
+        userAppointments.setFont(new java.awt.Font("Tamil MN", 1, 14)); // NOI18N
+        userAppointments.setText("View Appointments");
+        userAppointments.setPreferredSize(new java.awt.Dimension(180, 24));
+        userAppointments.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                userAppointmentsActionPerformed(evt);
+            }
+        });
+
+        userLogout.setBackground(new java.awt.Color(255, 51, 51));
+        userLogout.setFont(new java.awt.Font("Tamil MN", 1, 14)); // NOI18N
+        userLogout.setText("Logout");
+        userLogout.setPreferredSize(new java.awt.Dimension(180, 24));
+        userLogout.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                userLogoutActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setIcon(new javax.swing.ImageIcon("/Users/anamikaverma/Desktop/Images/graduation-2897164_1280 (1).jpeg")); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30))
+            .addComponent(userSearchJobs, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(userViewApplications, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(userAppointments, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(userLogout, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(jLabel4)
+                .addContainerGap(33, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(jLabel1)
+                .addGap(32, 32, 32)
+                .addComponent(userSearchJobs, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(userViewApplications)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(userAppointments, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(userLogout, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(69, 69, 69)
+                .addComponent(jLabel4)
+                .addContainerGap(94, Short.MAX_VALUE))
+        );
+
+        jSplitPane1.setLeftComponent(jPanel1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jSplitPane1)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jSplitPane1)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void userViewApplicationsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userViewApplicationsActionPerformed
+        ViewJobApplicationsPanel vjap= new ViewJobApplicationsPanel();
+        jSplitPane1.setRightComponent(vjap);
+    }//GEN-LAST:event_userViewApplicationsActionPerformed
+
+    private void userSearchJobsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userSearchJobsActionPerformed
+        searchJobsPanel sjp= new searchJobsPanel();
+        jSplitPane1.setRightComponent(sjp);
+    }//GEN-LAST:event_userSearchJobsActionPerformed
+
+    private void userAppointmentsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userAppointmentsActionPerformed
+        ViewAppointmentsPanel vap=new ViewAppointmentsPanel();
+        jSplitPane1.setRightComponent(vap);
+    }//GEN-LAST:event_userAppointmentsActionPerformed
+
+    private void userLogoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userLogoutActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_userLogoutActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +245,17 @@ public class UserLandingP extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JSplitPane jSplitPane1;
+    private javax.swing.JButton userAppointments;
+    private javax.swing.JButton userLogout;
+    private javax.swing.JButton userSearchJobs;
+    private javax.swing.JButton userViewApplications;
     // End of variables declaration//GEN-END:variables
 }
--- AEDFinalProject2022/src/UserLogin/UserSnLogin.java
+++ AEDFinalProject2022/src/UserLogin/UserSnLogin.java
@@ -138,7 +138,7 @@ public class UserSnLogin extends javax.swing.JFrame {
                ResultSet Rs=St.executeQuery();
                if(Rs.next()){
                    JOptionPane.showMessageDialog(this, "Login Successful!"); 
-                   UserLandingPage ulp =new UserLandingPage();
+                   UserLandingP ulp =new UserLandingP();
                    ulp.setVisible(true);
                    this.dispose();
                }else{
@@ -182,7 +182,6 @@ public class UserSnLogin extends javax.swing.JFrame {
         userForget = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("STIXIntegralsSm", 1, 18)); // NOI18N
         jLabel1.setText("New here? Sign up below");
@@ -282,7 +281,7 @@ public class UserSnLogin extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(signPass)
                                 .addGap(4, 4, 4))
-                            .addComponent(signUsername, javax.swing.GroupLayout.DEFAULT_SIZE, 268, Short.MAX_VALUE)))
+                            .addComponent(signUsername)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(btnSignup)
@@ -295,7 +294,7 @@ public class UserSnLogin extends javax.swing.JFrame {
                                 .addGap(55, 55, 55)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(jPanel1Layout.createSequentialGroup()
-                                        .addComponent(uniCombo, 0, 260, Short.MAX_VALUE)
+                                        .addComponent(uniCombo, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addGap(8, 8, 8))
                                     .addGroup(jPanel1Layout.createSequentialGroup()
                                         .addComponent(signContact)
@@ -311,7 +310,7 @@ public class UserSnLogin extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(12, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -371,8 +370,6 @@ public class UserSnLogin extends javax.swing.JFrame {
                     .addComponent(signReset))
                 .addContainerGap(89, Short.MAX_VALUE))
         );
-
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 590, 700));
 
         jPanel2.setBackground(new java.awt.Color(153, 153, 255));
 
@@ -446,7 +443,20 @@ public class UserSnLogin extends javax.swing.JFrame {
                 .addContainerGap(234, Short.MAX_VALUE))
         );
 
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(589, 0, -1, 700));
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(589, 589, 589)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
 
         pack();
         setLocationRelativeTo(null);
